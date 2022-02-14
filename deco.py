6fe30fe0b5dfbca6e6ea8e1db616cd532392c23b
yt.kim <qndbsxo@naver.com>
2022-02-14 14:13:50 +0900

    test

---
+++ deco.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
+
+## Default JSON encoder를 set를 JSON으로 변환할 수 없다.
+## 그럼으로 커스텀 엔코더를 작성해서 set을 list로 변환하여
+## JSON으로 변환 가능하게 해주어야 한다.
+class CustomJSONEncoder(JSON):
+	def defy
